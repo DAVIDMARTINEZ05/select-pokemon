bf3b6e7949b58c7219a11ad54e2640eef9adde6a
DAVIDMARTINEZ05 <davidmtzmnd5@gmail.com>
2024-11-16 12:49:35 +0100

    ejercicioo acabado

--- js/script.js
+++ js/script.js
@@ -1,33 +1,30 @@
-const pokemons = document.getElementById("pokemon-select")
-const btnPkm = document.getElementById("get-pokemon")
-
-btnPkm.addEventListener("click",()=>{
-    const pokemonNombre = pokemons.value
-    const api = `https://pokeapi.co/api/v2/pokemon/${pokemonNombre}`
-    
-
-    fetch(api)
-    .then((response)=>{
-        if(!response.ok){
-           console.log('La solicitud no fue exitosa');
-        }
-        return response.json()
-    })
-   .then((data)=>{
-    const  informacionPokemon= document.createElement("div")
-    informacionPokemon.classList.add("infoPokemon")
-     document.querySelector(".pokemon-form").insertAdjacentElement("afterend",informacionPokemon)
-     informacionPokemon.innerHTML=`
+const pokemonSelect = document.getElementById('pokemon-select');
+const getPokemon = document.getElementById('get-pokemon');
+getPokemon.addEventListener('click', () => {
+  const nombrePokemon = pokemonSelect.value;
+  const urlApi = `https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`;
+  fetch(urlApi)
+  .then((response) => {
+    if(!response.ok) {
+      throw new Error('La solicitud no fue exitosa');
+    }
+    return response.json();
+  })
+  .then((data) => {
+    let infoContainer = document.querySelector('.info-container');
+    if (!infoContainer) {
+      infoContainer = document.createElement('div');
+      infoContainer.classList.add('info-container');
+      document.querySelector('.pokemon-form').insertAdjacentElement('afterend', infoContainer);
+    }
+    infoContainer.innerHTML = '';
+    infoContainer.innerHTML = `
+      <h2> ${data.name}</h2>
       <img src="${data.sprites.other["official-artwork"].front_default}" alt="${data.name}" id="pokemon">
-      <h2>Nombre: ${data.name}</h2>
       <h4>Tipo: ${data.types.map(type => type.type.name).join(", ")}</h4>
       <h4>Altura: ${data.height}</h4>
       <h4>Peso: ${data.weight}</h4>
       `;
-  }) .catch((error) => {
-    area.innerText = 'error 404';
-  });
-
-
-   
-})
+  })
+  .catch(error => console.error(error));
+});
